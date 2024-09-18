fed642a9d3e19f1eaed5bc1d33359436b0c1d8a0
Youngjoo <tyty4420@naver.com>
2024-09-19 08:04:14 +0900

    Initial commit

---
+++ src/main/java/wanted/mobinity/MobinityApplication.java
@@ -0,0 +1,13 @@
+package wanted.mobinity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobinityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobinityApplication.class, args);
+	}
+
+}
